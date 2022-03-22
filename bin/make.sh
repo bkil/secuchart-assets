68575cf77b94826922c58241e0fce906d5715fdf
bkil <b.K.il.h.u+tigbuh@gmail.com>
2022-03-22 01:52:02 +0100

    bin/make.sh: link non-images differently

--- bin/make.sh
+++ bin/make.sh
@@ -21,21 +21,20 @@ tee sitemap.txt |
   <title>SecuChart-assets</title>
   <link rel='shortcut icon' type=image/x-icon href=data:image/x-icon;,>
 <body>
-<a href='https://bkil.gitlab.io/secuchart/#screenshots'>SecuChart</a>
+<a href='https://bkil.gitlab.io/secuchart/#screenshots'>=&gt; SecuChart</a>
 EOF
 
 while read F; do
-  IMG="<br><img src='$F'>"
 
-  echo "$F" |
-  grep -qE "(\.(txt|md|html|sh)|^LICENSE)$" &&
-    IMG=""
+  if
+    echo "$F" |
+    grep -qE "(\.(txt|md|html|sh)|^LICENSE)$"
+  then
+    echo "<br><a href='$F'>$F</a>"
+  else
+    echo "<br><p id='$F'><a href='#$F'>#</a> $F</p><br><img src='$F'>"
+  fi
 
-  cat << EOF
-<br>
- <p id='$F'><a href='#$F'>$F</a></p>
- $IMG
-EOF
 done
 
 cat << EOF
